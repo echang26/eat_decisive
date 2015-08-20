68b42a83ebe735f39cc07f9687dfa50bf20c26d7
Emily Chang <echang26@gmail.com>
2015-08-19 23:33:28 -0400

    updated settings

--- eat_decisive/settings.py
+++ eat_decisive/settings.py
@@ -145,6 +145,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # Absolute path to the media direct
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
 
+DATABASES['default']['ENGINE'] = 'django_postgrespool'
+
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
