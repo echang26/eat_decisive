4b58372f4c27b9aace4f1324c20e48aff24281fe
Emily Chang <echang26@gmail.com>
2015-08-20 20:45:54 -0400

    changing to cling

--- eat_decisive/wsgi.py
+++ eat_decisive/wsgi.py
@@ -11,12 +11,12 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eat_decisive.settings")
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+#from whitenoise.django import DjangoWhiteNoise
 from dj_static import Cling
 
-application = get_wsgi_application()
+#application = get_wsgi_application()
 
-#application = Cling(get_wsgi_application())
+application = Cling(get_wsgi_application())
 
 
-application = DjangoWhiteNoise(application)
+#application = DjangoWhiteNoise(application)
