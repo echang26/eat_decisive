a1a3701fcfbd8a41939d3e3660e49a08fc8e8812
Emily Chang <echang26@gmail.com>
2015-08-21 17:27:23 -0400

    adding backslash to goodreads login url

--- eat_decisive/urls.py
+++ eat_decisive/urls.py
@@ -4,22 +4,7 @@ from eat_decisive import views
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
-'''
-urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'eat_decisive.views.index', name='eat_index'),
-    url(r'^getdecision/', views.submit_query, name='decision'),
-    url(r'^login/', views.login, name='login'),
-    url(r'^readdecisive/', views.readdecisive, name='readdecisive'),
-    url(r'^goodreadslogin/', views.goodreads_login, name='goodreadslogin'),
-    url(r'^readdecisivemember/', views.readdecisivemember, name='readdecisivemember'),
-    url(r'^popularbookgenerator/', views.random_popular_book, name='randompopularbook'),
-    url(r'^whattoreadnext/', views.random_to_read, name='randomtoread'),
-#    url(r'^goodreadslink/', views.goodreads, name='goodreads'),
-#    url(r'^oauth2/', include('provider.oauth2.urls', namespace = 'oauth2')),
 
-)
-'''
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.index, name = 'eat_index'),
