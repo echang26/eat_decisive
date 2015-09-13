2f9b9cf78052a0766424e0fd8d809f4b811879ed
Emily Chang <echang26@gmail.com>
2015-09-13 19:46:57 -0400

    updating urlpatterns

--- eat_decisive/urls.py
+++ eat_decisive/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from eat_decisive import views
+#from eat_decisive import views
 from django.conf import settings
 from django.conf.urls.static import static
 import views
