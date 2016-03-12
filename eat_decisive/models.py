36ad07bd9df0cbe50b81d45e4b51c701dd2122e9
echang26 <echang26@Gmail.com>
2016-03-11 23:05:18 -0500

    Update models.py

--- eat_decisive/models.py
+++ eat_decisive/models.py
@@ -3,8 +3,6 @@ from django.forms import ModelForm
 
 class LatestQuery(models.Model):
     searched = models.CharField(max_length=128)
-#    def save(self, *args, **kwargs):
-#        super(LatestQuery, self).save(*args, **kwargs)
 
     def __unicode__(self):      #For Python 2, use __str__ on Python 3
         return self.searched
